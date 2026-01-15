1d2e6ce0e5c95c6608bfa32066555c8ff08845bc
alexn <alexnyazev@gmail.com>
2026-01-15 17:28:41 +0300

    1

--- 15/src/routes/sessions.js
+++ 15/src/routes/sessions.js
@@ -4,6 +4,29 @@ export default (app) => {
   const users = generateUsers();
 
   // BEGIN (write your solution here)
+ app.get("/sessions/new", (req, res) => {
+    res.view("src/views/sessions/new", { error: null });
+  });
 
+  app.post("/sessions", (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find((user) => user.username === username);
+
+    if (!user) {
+      return res.view("src/views/sessions/new", { error: "User not found" });
+    }
+
+    if (decrypt(user.password) !== password) {
+      return res.view("src/views/sessions/new", { error: "Wrong password" });
+    }
+
+    req.session.username = user.username;
+    return res.redirect("/");
+  });
+
+  app.post("/sessions/delete", (req, res) => {
+    req.session.username = null;
+    res.redirect("/");
+  });
   // END
 };
